e402fb92b7dc6656de7d78bb35ab39ecc51a6171
Ruslan <89604587433@mail.ru>
2022-09-01 14:57:10 +0300

    Вывод диапазона чисел

---
+++ Task_4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Ведите число");
+int namber = int.Parse(Console.ReadLine());
+int count=Math.Abs(namber)*(-1);
+while(count<namber+1)
+    {
+        Console.Write($"{count}  ");
+        count++;
+    }
