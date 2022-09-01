bb0991e1a81faeede540658addee7de8fc75c48a
Ruslan <89604587433@mail.ru>
2022-09-01 07:48:54 +0300

    Квадрат числа

---
+++ MyProject/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Ведите число");
+int namber = int.Parse(Console.ReadLine());
+// Получение квадрата числа
+int result = namber*namber;
+Console.WriteLine($"Квадратчисла {namber} = {result}");
+int result2=Convert.ToInt32(Math.Pow(namber,2));
+Console.WriteLine($"Квадратчисла {namber} = {result}");
