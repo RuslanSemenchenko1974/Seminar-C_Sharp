539b7cb3c61bb698f4d2d58484f8363e9a9212e3
Ruslan <89604587433@mail.ru>
2022-09-01 09:47:40 +0300

    Сравнение Дней

---
+++ Task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Ведите Day");
+string dayN = Console.ReadLine();
+if (dayN=="5")
+{
+    Console.WriteLine("Пятница, Friday");
+}
+if (dayN=="3")
+{
+    Console.WriteLine("Среда, Wednesday");
+}
