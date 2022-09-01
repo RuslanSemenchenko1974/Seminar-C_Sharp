8c10c45e98fad40121d6728f85c01ea10d1d9f49
Ruslan <89604587433@mail.ru>
2022-09-01 09:32:12 +0300

    Сравнение Квадрата числа

---
+++ Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ведите число 1");
+int namber1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ведите число 1");
+int namber2 = int.Parse(Console.ReadLine());
+if (namber1==Convert.ToInt32(Math.Pow(namber2,2)))
+{
+    Console.WriteLine($"Число {namber1} квадрат {namber2}");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
